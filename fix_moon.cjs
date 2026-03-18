c582baa0619c26ae5c8110b5848b36239388ee56
subhamagarrwal <130570909+subhamagarrwal@users.noreply.github.com>
2026-03-19 01:02:29 +0530

    content updation left

--- fix_moon.cjs
+++
@@ -1,8 +0,0 @@
-const fs=require('fs'); 
-let f = fs.readFileSync('src/components/MountainLandscape.tsx', 'utf8'); 
-f = f.replace(/style\.setProperty\('--sun-x',[^;]*;/g, 'style.setProperty(\'--sun-x\', \\%\);');
-f = f.replace(/style\.setProperty\('--sun-y',[^;]*;/g, 'style.setProperty(\'--sun-y\', \\%\);');
-f = f.replace(/style\.setProperty\('--sun-angle',[^;]*;/g, 'style.setProperty(\'--sun-angle\', \\deg\);');
-f = f.replace(/style\.setProperty\('--moon-x',[^;]*;/g, 'style.setProperty(\'--moon-x\', \\%\);');
-f = f.replace(/style\.setProperty\('--moon-y',[^;]*;/g, 'style.setProperty(\'--moon-y\', \\%\);');
-fs.writeFileSync('src/components/MountainLandscape.tsx', f);
--- fix_tabs.js
+++
@@ -1,6 +0,0 @@
-﻿const fs = require('fs');
-const path = require('path');
-const file = 'src/components/JourneySection.tsx';
-let c = fs.readFileSync(file, 'utf8');
-c = c.replace(/data-\[state=active\]:text-white/g, "data-[state=active]:text-[var(--btn-text-color)]");
-fs.writeFileSync(file, c);
